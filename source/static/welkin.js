dc59edea2cc28f896267d2a1909337f71998bacf
wzp811 <811235852@qq.com>
2023-09-05 00:11:16 +0800

    add js and fix global.css

--- source/static/welkin.js
+++ source/static/welkin.js
@@ -1,3 +1,4 @@
+// 解决firefox中刷新页面触发transition
 window.addEventListener("DOMContentLoaded", function() {
     var header_top = document.getElementsByClassName('header-top')[0];
     requestAnimationFrame(function() {
@@ -5,8 +6,8 @@ window.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// 上下滚动页面header-top动画
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction () {
     // if (document.getElementsByClassName("posts-container"[0].scrollTop > -120)) {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 280) {
